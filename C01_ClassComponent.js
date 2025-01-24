f7bbb1050929d298513e5122b765d2f5d91bf75b
Chetan Gosavi <chetangosavi81@gmail.com>
2025-01-25 01:11:31 +0530

    ReactBasics

---
+++ C01_ClassComponent.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react"
+
+class Welcome extends Component{
+    return(){
+        this.return(
+            <div>
+                Welcome to Class Component
+            </div>
+        )
+    }
+}
+
+export default Welcome;
